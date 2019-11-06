f6df4ee9a04873ce0e73b2736b84abb658cad97c
zhang.hp <coffee2007@126.com>
2019-11-06 10:04:40 +0800

    class loader

---
+++ oauth2-jwt-server/src/main/java/com/coffee/oauth/Oauth2JwtServerApplication.java
@@ -0,0 +1,16 @@
+package com.coffee.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zhang.hp on 2019/11/6.
+ */
+@SpringBootApplication
+public class Oauth2JwtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2JwtServerApplication.class, args);
+    }
+
+}
---
+++ oauth2-jwt-server/src/test/java/com/coffee/oauth/Oauth2ServerApplicationTests.java
@@ -0,0 +1,21 @@
+package com.coffee.oauth;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author zhang.hp
+ * @date 2019/11/6.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2ServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
